3db5c6e37ea18fc72f44319651f0aade4c349283
rohan km <rohankm890@gmail.com>
2023-09-14 21:38:13 +0530

    added useInfinitequery hook

---
+++ example/app/databases/infiniteQuery/page.tsx
@@ -0,0 +1,181 @@
+"use client";
+
+import React, { useEffect, useMemo, useState } from "react";
+import {
+  useDocument,
+  useCollection,
+  Query,
+  useDocumentUpdate,
+  DatabaseDocument,
+  useDocumentDelete,
+  useDocumentCreate,
+} from "react-appwrite/databases";
+import { FiTrash } from "react-icons/fi";
+
+type Article = DatabaseDocument<{
+  title: string;
+  content: string;
+  published: boolean;
+}>;
+
+function InfiniteQuery() {
+  // const createDocument = useDocumentCreate<Article>();
+  // const { data: article } = useDocument<Article>(
+  //   "test",
+  //   "articles",
+  //   "643866c6f386c9c982c1"
+  // );
+  const data = useCollection<Article>("default", "dictionary");
+
+  // const { data: publishedArticles } = useCollection<Article>(
+  //   "default",
+  //   "dictionary"
+  // );
+
+  // const { data: unpublishedArticles } = useCollection<Article>(
+  //   "test",
+  //   "articles",
+  //   [Query.notEqual<Article>("published", true)]
+  // );
+
+  // console.log(publishedArticles);
+
+  // useEffect(() => {
+  //   if (article) {
+  //     console.log("Document changed", article);
+  //   }
+  // }, [article]);
+
+  useEffect(() => {
+    console.log("Collection changed", data);
+    console.log(data.flattenData);
+  }, [data]);
+
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [published, setPublished] = useState<boolean>(true);
+
+  return (
+    <div className="flex items-center justify-center flex-1 gap-4">
+      <div className="flex flex-col gap-4 md:flex-row ">
+        <h1>Infinite Loading</h1>
+
+        <div>
+          <button
+            onClick={() => data.fetchPreviousPage()}
+            disabled={!data.hasPreviousPage || data.isFetchingPreviousPage}
+          >
+            {data.isFetchingPreviousPage
+              ? "Loading more..."
+              : data.hasPreviousPage
+              ? "Load Older"
+              : "Nothing more to load"}
+          </button>
+        </div>
+        <p>{data.flattenData.documents?.length}</p>
+        {/* {data.pages.map((page) => (
+          <React.Fragment key={page.nextId}>
+            {page.data.map((project) => (
+              <p
+                style={{
+                  border: "1px solid gray",
+                  borderRadius: "5px",
+                  padding: "10rem 1rem",
+                  background: `hsla(${project.id * 30}, 60%, 80%, 1)`,
+                }}
+                key={project.id}
+              >
+                {project.name}
+              </p>
+            ))}
+          </React.Fragment>
+        ))} */}
+        <div>
+          <button
+            onClick={() => data.fetchNextPage()}
+            disabled={!data.hasNextPage || data.isFetchingNextPage}
+          >
+            {data.isFetchingNextPage
+              ? "Loading more..."
+              : data.hasNextPage
+              ? "Load Newer"
+              : "Nothing more to load"}
+          </button>
+        </div>
+        <div>
+          {data.isFetching && !data.isFetchingNextPage
+            ? "Background Updating..."
+            : null}
+        </div>
+      </div>
+      {/* <form
+          className="flex flex-col gap-4 p-4 text-black bg-white rounded-sm"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            await createDocument.mutateAsync({
+              databaseId: "test",
+              collectionId: "articles",
+              data: {
+                title: title,
+                content: content,
+                published: published,
+              },
+            });
+            setTitle("");
+            setContent("");
+            setPublished(true);
+          }}
+        >
+          <span>Create Article</span>
+          <input
+            type="text"
+            placeholder="Title"
+            className="text-white"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <textarea
+            placeholder="Content"
+            className="text-white"
+            value={content}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+          />
+          <label className="flex flex-row items-center gap-2">
+            <input
+              type="checkbox"
+              checked={published}
+              onChange={(e) => {
+                setPublished(e.target.checked);
+              }}
+            />
+            Published
+          </label>
+          <button
+            onClick={() => {}}
+            className="w-full p-2 bg-blue-500 rounded-md"
+          >
+            Submit
+          </button>
+        </form>
+        <div className="flex flex-col items-center justify-start gap-4">
+          <h2 className="font-bold">Published Articles</h2>
+          {publishedArticles?.documents?.map((article: Article) => (
+            <ArticleListItem key={article.$id} {...article} />
+          ))}
+        </div>
+        <div className="flex flex-col items-center justify-start gap-4">
+          <h2 className="font-bold">Unpublished Articles</h2>
+          {unpublishedArticles?.documents?.map((article: Article) => (
+            <ArticleListItem key={article.$id} {...article} />
+          ))}
+        </div> */}
+    </div>
+    // </div>
+  );
+}
+
+export default InfiniteQuery;
--- src/databases/useCollection.ts
+++ src/databases/useCollection.ts
@@ -1,79 +1,164 @@
-'use client'
+"use client";
 
-import { useQuery, useQueryClient, type UseQueryOptions } from '@tanstack/react-query'
-import { Models } from 'appwrite'
-import { useEffect, useMemo } from 'react'
-import { useAppwrite } from 'react-appwrite'
-import type { DatabaseDocumentOperation, DatabaseDocument, DatabaseCollection } from 'react-appwrite/databases/types'
+import {
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+  useQuery,
+  useQueryClient,
+  type UseQueryOptions,
+} from "@tanstack/react-query";
+import { Models } from "appwrite";
+import { useEffect, useMemo } from "react";
+import { Query, useAppwrite } from "react-appwrite";
+import type {
+  DatabaseDocumentOperation,
+  DatabaseDocument,
+  DatabaseCollection,
+} from "react-appwrite/databases/types";
 
 /**
  * Fetches a collection from a database.
  * @param databaseId The database the collection belongs to.
  * @param collectionId The collection to fetch.
  * @param queries Queries to filter the collection by.
- * @param options Options to pass to `react-query`.
+ * @param options Options to configure query.
+ * @param queryOptions Options to pass to `react-query`.
  * @link [Appwrite Documentation](https://appwrite.io/docs/client/databases?sdk=web-default#databasesListDocuments)
  */
 export function useCollection<TDocument>(
   databaseId: string,
   collectionId: string,
   queries: string[] = [],
-  options?: UseQueryOptions<Models.DocumentList<DatabaseDocument<TDocument>>, unknown, Models.DocumentList<DatabaseDocument<TDocument>>, (string | {
-    queries: string[],
-  })[]>
+  options = {
+    loadAll: false,
+    addFirst: false,
+    realtime: true,
+  },
+  queryOptions?: UseInfiniteQueryOptions<
+    Models.DocumentList<DatabaseDocument<TDocument>>,
+    unknown
+  >
 ) {
-  const { databases } = useAppwrite()
-  const queryClient = useQueryClient()
-  const queryKey = useMemo(() => ['appwrite', 'databases', databaseId, collectionId, { queries }], [databaseId, collectionId, queries])
-  const queryResult = useQuery({
+  const { databases } = useAppwrite();
+  const queryClient = useQueryClient();
+  const queryKey = useMemo(
+    () => ["appwrite", "databases", databaseId, collectionId, { queries }],
+    [databaseId, collectionId, queries]
+  );
+  const queryResult = useInfiniteQuery({
     queryKey,
-    queryFn: async () => {
-      return await databases.listDocuments<DatabaseDocument<TDocument>>(databaseId, collectionId, queries)
-    },
-
-    onSuccess: collection => {
-      for (const document of collection.documents) {
-        queryClient.setQueryData(['appwrite', 'databases', databaseId, collectionId, 'documents', document.$id], document)
+    queryFn: async (param) => {
+      console.log("param", param);
+      if (!param.pageParam)
+        return await databases.listDocuments<DatabaseDocument<TDocument>>(
+          databaseId,
+          collectionId,
+          queries
+        );
+      else {
+        return await databases.listDocuments<DatabaseDocument<TDocument>>(
+          databaseId,
+          collectionId,
+          [
+            ...queries,
+            param.pageParam.type == "getNextPage"
+              ? Query.cursorAfter(param.pageParam.cursor)
+              : Query.cursorBefore(param.pageParam.cursor),
+          ]
+        );
       }
     },
 
-    ...options,
-  })
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.documents.length == 0
+        ? undefined
+        : {
+            cursor: lastPage.documents[lastPage.documents.length - 1].$id,
+            type: "getNextPage",
+          },
 
+    getPreviousPageParam: (firstPage, allPages) =>
+      firstPage.documents.length == 0
+        ? undefined
+        : { cursor: firstPage.documents[0].$id, type: "getPreviousPage" },
+
+    onSuccess: (collection) => {
+      for (const document of collection.pages[collection.pages.length - 1]
+        .documents) {
+        queryClient.setQueryData(
+          [
+            "appwrite",
+            "databases",
+            databaseId,
+            collectionId,
+            "documents",
+            document.$id,
+          ],
+          document
+        );
+      }
+    },
+    ...queryOptions,
+  });
+  console.log(options);
   useEffect(() => {
-    const unsubscribe = databases.client.subscribe(`databases.${databaseId}.collections.${collectionId}.documents`, response => {
-      const [, operation] = response.events[0].match(/\.(\w+)$/) as RegExpMatchArray
-      const document = response.payload as DatabaseDocument<TDocument>
+    if (options.realtime) {
+      const unsubscribe = databases.client.subscribe(
+        `databases.${databaseId}.collections.${collectionId}.documents`,
+        (response) => {
+          const [, operation] = response.events[0].match(
+            /\.(\w+)$/
+          ) as RegExpMatchArray;
+          const document = response.payload as DatabaseDocument<TDocument>;
 
-      switch (operation as DatabaseDocumentOperation) {
-        case 'create':
-        case 'update':
-        case 'delete':
-          queryClient.setQueryData(['appwrite', 'databases', databaseId, collectionId, 'documents', document.$id], document)
+          switch (operation as DatabaseDocumentOperation) {
+            case "create":
+            case "update":
+            case "delete":
+              queryClient.setQueryData(
+                [
+                  "appwrite",
+                  "databases",
+                  databaseId,
+                  collectionId,
+                  "documents",
+                  document.$id,
+                ],
+                document
+              );
 
-          // This is not optimal, but is needed until this is implemented.
-          // https://github.com/appwrite/appwrite/issues/2490
-          queryClient.invalidateQueries({
-            queryKey,
-            exact: true,
-          })
+              // This is not optimal, but is needed until this is implemented.
+              // https://github.com/appwrite/appwrite/issues/2490
+              queryClient.invalidateQueries({
+                queryKey,
+                exact: true,
+              });
 
-          break
-        // case 'delete':
-        //   queryClient.setQueryData<Models.DocumentList<DatabaseDocument<TDocument>>>(queryKey, collection => {
-        //     if (collection?.documents) {
-        //       return collection.documents.filter(storedDocument => storedDocument.$id !== document.$id)
-        //     }
+              break;
+            // case 'delete':
+            //   queryClient.setQueryData<Models.DocumentList<DatabaseDocument<TDocument>>>(queryKey, collection => {
+            //     if (collection?.documents) {
+            //       return collection.documents.filter(storedDocument => storedDocument.$id !== document.$id)
+            //     }
 
-        //     return collection
-        //   })
+            //     return collection
+            //   })
 
-        //   break
-      }
-    })
+            //   break
+          }
+        }
+      );
 
-    return unsubscribe
-  }, [databaseId, collectionId, queryKey])
+      return unsubscribe;
+    }
+    return;
+  }, [databaseId, collectionId, queryKey, options]);
 
-  return queryResult
+  const flattenData = useMemo(() => {
+    return {
+      documents: queryResult?.data?.pages.flatMap((page) => page.documents),
+      total: queryResult?.data?.pages[0].total,
+    };
+  }, [queryResult.data]);
+  return { ...queryResult, flattenData };
 }
